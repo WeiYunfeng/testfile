84ec9422bbbbebcced54a76c33ebbad403c65666
WeiYunfeng <905577137@qq.com>
2024-01-19 12:25:36 +0800

    first develop

--- testfile.go
+++ testfile.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+func Add(a, b int) int {
+	return a + b
+}
+
 func main() {
+	var Add int = Add(2, 3)
+	fmt.Printf("Add: %v\n", Add)
 	fmt.Println("第一次修改")
 	fmt.Println("第二次修改")
 	fmt.Println("第三次修改")
